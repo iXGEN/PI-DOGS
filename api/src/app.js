862a60d14acd24922646b07c99a7c98d633f2833
Ignacio <ignadevlearning@gmail.com>
2023-01-04 15:45:47 -0300

    *

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,8 @@ server.use((req, res, next) => {
   /* res.header('Access-Control-Allow-Origin', 'http://localhost:3000') */ // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Origin",
-    "https://pi-dogs-two-inky.vercel.app"
+    //"https://pi-dogs-two-inky.vercel.app"
+    "*"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
