2d579b12c7c86c82731c6507c3aa1eed7eddf5f1
Ignacio <ignadevlearning@gmail.com>
2023-01-05 23:34:02 -0300

    a

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ import CreateDog from "./components/CreateDog/CreateDog";
 import { useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import axios from "axios";
-axios.defaults.baseURL = "https://pi-dogs-production-c336.up.railway.app/";
+/* axios.defaults.baseURL = "https://pi-dogs-production-c336.up.railway.app/"; */
+axios.defaults.baseURL = "http://localhost:3001/";
 
 function App() {
   const location = useLocation();
